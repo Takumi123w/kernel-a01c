78e3d37664a438aafbe0f420197936912f7da4da
Takumi123w <166905091+Takumi123w@users.noreply.github.com>
2025-12-03 19:22:34 +0700

    Update build_kernel.sh

--- build_kernel.sh
+++ build_kernel.sh
@@ -8,8 +8,16 @@ else
 curl -LSs "https://raw.githubusercontent.com/KernelSU-Next/KernelSU-Next/next/kernel/setup.sh" | bash -
 fi
 
+#########################
+# KERNELSU Configuration#
+#########################
+
 # Replace arch for 32bit support
-cp -rf $(pwd)/arch.h $(pwd)/drivers/kernelsu/kernel
+cp -rf $(pwd)/arch.h $(pwd)/KernelSU-Next/kernel
+# Replace path for allowlist for A01 Core fix bugs
+sed -i 's|#define KERNEL_SU_ALLOWLIST "/data/adb/ksu/.allowlist"|#define KERNEL_SU_ALLOWLIST "/data/.allowlist"|g' $(pwd)/KernelSU-Next/kernel/allowlist.c
+# Write permission for file .allowlist change to 664 for writeable file
+sed -i 's|ksu_filp_open_compat(KERNEL_SU_ALLOWLIST, O_WRONLY | O_CREAT | O_TRUNC, 0644);|ksu_filp_open_compat(KERNEL_SU_ALLOWLIST, O_WRONLY | O_CREAT | O_TRUNC, 0664);|g' $(pwd)/KernelSU-Next/kernel/allowlist.c
 
 export CROSS_COMPILE=$(pwd)/arm-linux-androideabi-4.9/bin/arm-linux-androidkernel-
 export CC=$(pwd)/arm-linux-androideabi-4.9/bin/arm-linux-androidkernel-gcc
