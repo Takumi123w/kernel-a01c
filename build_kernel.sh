c97ed60e1b8f192b6fb8d2d003ca7a7775111af5
Takumi123w <166905091+Takumi123w@users.noreply.github.com>
2025-12-19 04:09:01 +0700

    Replace repo with Rissu KSU

--- build_kernel.sh
+++ build_kernel.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-if [ -e $(pwd)/KernelSU-Next ]
+if [ -e $(pwd)/KernelSU ]
 echo "KSU exist skip Download KSU"
 else
-curl -LSs "https://raw.githubusercontent.com/Takumi123w/KernelSU-Next-A01core/stable/kernel/setup.sh" | bash -
+curl -LSs "https://raw.githubusercontent.com/rsuntk/KernelSU/main/kernel/setup.sh" | bash -s v3.0.0-30-legacy
 fi
 
 export CROSS_COMPILE=$(pwd)/arm-linux-androideabi-4.9/bin/arm-linux-androidkernel-
