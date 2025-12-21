08226ddb447c41e7fd728d86632105d17b3338dc
Takumi123w <166905091+Takumi123w@users.noreply.github.com>
2025-12-21 13:40:00 +0700

    Enhance build_kernel.sh for AnyKernel3 repackaging
    Add timestamped kernel repackaging in AnyKernel3.

--- build_kernel.sh
+++ build_kernel.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+TIME=$(date +%Y%m%d-%H%M)
+
+if [ -e $(pwd)/AnyKernel3 ]
+then
+echo "AnyKernel exist skip Download AnyKernel3"
+else
+git clone https://github.com/Takumi123w/AnyKernel3.git
+fi
 
 export CROSS_COMPILE=$(pwd)/arm-linux-androideabi-4.9/bin/arm-linux-androidkernel-
 export CC=$(pwd)/arm-linux-androideabi-4.9/bin/arm-linux-androidkernel-gcc
@@ -13,3 +21,9 @@ make -C $(pwd) O=$(pwd)/out KCFLAGS=-w CONFIG_SECTION_MISMATCH_WARN_ONLY=y -j16
 
 cp out/arch/arm/boot/Image $(pwd)/arch/arm/boot/Image
 cp out/arch/arm/boot/Image $(pwd)/arch/arm64/boot/Image
+
+# AnyKernel3 Repack
+rm -rf $(pwd)/AnyKernel3/Image
+cp out/arch/arm/boot/Image $(pwd)/AnyKernel3/Image
+cd AnyKernel3
+zip -r9 "Kernel-Repack-$TIME.zip" * -x .git README.md *placeholder
